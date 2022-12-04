2f46f24e6e0cce6bf4a0bdab71256d15d9b3e198
skarkii <lucas@kuja-halkola.com>
2022-12-04 17:09:18 +0100

    day 4

---
+++ 04/task_1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+
+    std::ifstream inpFile("input");
+    std::string line;
+
+    int occasions = 0;
+
+    while(getline(inpFile, line))
+    {
+        std::string firstElf = line.substr(0, line.find(','));
+        std::string secoundElf = line.substr(line.find(',') + 1, line.length());
+    
+        int feBegin = stoi(firstElf.substr(0, firstElf.find('-')));
+        int feEnd = stoi(firstElf.substr(firstElf.find('-') + 1, firstElf.length()));
+
+        int seBegin = stoi(secoundElf.substr(0, secoundElf.find('-')));
+        int seEnd = stoi(secoundElf.substr(secoundElf.find('-') + 1, secoundElf.length()));
+
+        if((feBegin >= seBegin && feBegin <= seEnd && feEnd >= seBegin && feEnd <= seEnd)
+        or (seBegin >= feBegin && seEnd <= feEnd && seEnd >= feBegin && seEnd <= feEnd))
+        {
+            occasions += 1;
+        }
+    }
+    std::cout << "Result : " << occasions << std::endl;
+
+    return 0;
+}
